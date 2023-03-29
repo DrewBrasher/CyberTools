a1f3f3fe802090fb3d2688432a166b48c081c257
Drew Brasher <abrasher79@gmail.com>
2023-03-29 18:25:32 -0500

    Added command line parameters to create file from google search or domain list in a text file

--- CyberTools.Console/Program.cs
+++ CyberTools.Console/Program.cs
@@ -2,36 +2,78 @@
 using CyberTools.WebSearch;
 using Whois;
 
-Console.WriteLine("Enter search query: ");
-var searchQuery = Console.ReadLine();
-var googleSearch = new GoogleSearchService();
 var whois = new WhoisLookup();
-var lines = new List<string>();
 
-// Get 100 results, 10 at a time, which is the max google allows.
-for (var i = 1; i < 11; i += 10)
+if (args == null || args.Length == 0 || args.Length % 2 != 0)
 {
-    var googleResults = googleSearch.Search(searchQuery, i, "m6"); // Get next page of results no older than 6 months
-    if (googleResults.Items == null) { break; }
-    foreach (var item in googleResults.Items)
+    Console.WriteLine("Invalid arguments"); // TODO: show documentation
+    return;
+}
+var argDictionary = new Dictionary<string, string>();
+for(var i = 0; i < args.Length; i += 2)
+{
+    argDictionary.Add(args[i], args[i + 1]);
+}
+
+if (argDictionary.ContainsKey("-searchQuery"))
+{
+    CreateFromSearch(argDictionary["-searchQuery"]);
+}
+
+if (argDictionary.ContainsKey("-fileName"))
+{
+    var domains = File.ReadAllLines(argDictionary["-fileName"]);
+    CreateFromDomains(domains);
+}
+
+void CreateFromSearch(string searchQuery)
+{
+    var googleSearch = new GoogleSearchService();
+    var lines = new List<string>();
+
+    // Get 100 results, 10 at a time, which is the max google allows.
+    for (var i = 1; i < 91; i += 10)
+    {
+        var googleResults = googleSearch.Search(searchQuery, i, "m6"); // Get next page of results no older than 6 months
+        if (googleResults.Items == null) { break; }
+        foreach (var item in googleResults.Items)
+        {
+            var lineParts = new List<string?>();
+            var domainName = item.DisplayLink.Replace("www.", "");
+            AddDomainInfo(lineParts, domainName);
+            lineParts.Add("Google Custom Search");
+            lineParts.Add(searchQuery);
+            lineParts.Add(item.Title);
+            lineParts.Add(item.Link);
+
+            lines.Add($"\"{string.Join("\",\"", lineParts)}\"");
+        }
+    }
+    File.WriteAllLines("SearchResults.csv", lines);
+}
+
+void CreateFromDomains(IList<string> domains)
+{
+    var lines = new List<string>();
+    foreach (var domainName in domains)
     {
         var lineParts = new List<string?>();
-        var domainName = item.DisplayLink.Replace("www.", "");
-        var iPAddresses = Dns.GetHostAddresses(domainName);
-        var whoisInfo = whois.Lookup(domainName);
-
-        lineParts.Add(domainName);
-        lineParts.Add(whoisInfo?.Registrar?.Name);
-        lineParts.Add(whoisInfo?.Registered == null ? "" : whoisInfo.Registered.Value.ToString("yyyy-MM-dd"));
-        lineParts.Add(iPAddresses.FirstOrDefault()?.ToString());
-        lineParts.Add(""); //TODO: get country
-        lineParts.Add(whoisInfo?.Registrar?.IanaId);
-        lineParts.Add(""); // TODO: get AS Organization
-
+        AddDomainInfo(lineParts, domainName);
 
         lines.Add($"\"{string.Join("\",\"", lineParts)}\"");
     }
 }
-File.WriteAllLines("SearchResults.csv", lines);
-//Console.ReadKey();
- 
+
+void AddDomainInfo(IList<string> lineParts, string domainName)
+{
+    var iPAddresses = Dns.GetHostAddresses(domainName);
+    var whoisInfo = whois.Lookup(domainName);
+
+    lineParts.Add(domainName);
+    lineParts.Add(whoisInfo?.Registrar?.Name);
+    lineParts.Add(whoisInfo?.Registered == null ? "" : whoisInfo.Registered.Value.ToString("yyyy-MM-dd"));
+    lineParts.Add(iPAddresses.FirstOrDefault()?.ToString());
+    lineParts.Add(""); //TODO: get country
+    lineParts.Add(whoisInfo?.Registrar?.IanaId);
+    lineParts.Add(""); // TODO: get AS Organization
+}
