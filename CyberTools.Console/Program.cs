cb279f646adc0b3b6b84d5780a1e411600b03644
Drew Brasher <abrasher79@gmail.com>
2023-04-19 16:32:41 -0500

    Moved csv code to a class library

--- CyberTools.Console/Program.cs
+++ CyberTools.Console/Program.cs
@@ -1,13 +1,6 @@
 ﻿using System.Net;
+using CyberTools.Files;
 using CyberTools.WebSearch;
-using IPinfo;
-using Whois;
-
-var whois = new WhoisLookup();
-var ipInfoToken = "MY_TOKEN"; // TODO: Move to appsettings.json
-var ipInfoClient = new IPinfoClient.Builder()
-    .AccessToken(ipInfoToken)
-    .Build();
 
 if (args == null || args.Length == 0 || args.Length % 2 != 0)
 {
@@ -20,69 +13,22 @@ for(var i = 0; i < args.Length; i += 2)
     argDictionary.Add(args[i], args[i + 1]);
 }
 
-if (argDictionary.ContainsKey("-searchQuery"))
+var csvService = new CsvService();
+
+if (argDictionary.ContainsKey("-searchToCsv"))
 {
-    CreateFromSearch(argDictionary["-searchQuery"]);
+    csvService.CreateFromSearch(argDictionary["-searchToCsv"]);
 }
 
-if (argDictionary.ContainsKey("-fileName"))
+if (argDictionary.ContainsKey("-domainsToCsv"))
 {
-    var domains = File.ReadAllLines(argDictionary["-fileName"]);
-    CreateFromDomains(domains);
+    var domains = File.ReadAllLines(argDictionary["-domainsToCsv"]);
+    csvService.CreateFromDomains(domains);
 }
 
-void CreateFromSearch(string searchQuery)
+if(argDictionary.ContainsKey("-googleAnnotations"))
 {
     var googleSearch = new GoogleSearchService();
-    var lines = new List<string>();
-
-    // Get 100 results, 10 at a time, which is the max google allows.
-    for (var i = 1; i < 91; i += 10)
-    {
-        var googleResults = googleSearch.Search(searchQuery, i, "m6"); // Get next page of results no older than 6 months
-        if (googleResults.Items == null) { break; }
-        foreach (var item in googleResults.Items)
-        {
-            var lineParts = new List<string?>();
-            var domainName = item.DisplayLink.Replace("www.", "");
-            AddDomainInfo(lineParts, domainName);
-            lineParts.Add("Google Custom Search");
-            lineParts.Add(searchQuery);
-            lineParts.Add(item.Title);
-            lineParts.Add(item.Link);
-
-            lines.Add($"\"{string.Join("\",\"", lineParts)}\"");
-        }
-    }
-    File.WriteAllLines("SearchResults.csv", lines);
-}
-
-void CreateFromDomains(IList<string> domains)
-{
-    var lines = new List<string>();
-    foreach (var domainName in domains)
-    {
-        var lineParts = new List<string?>();
-        AddDomainInfo(lineParts, domainName);
-
-        lines.Add($"\"{string.Join("\",\"", lineParts)}\"");
-    }
-    File.WriteAllLines("DomainInfo.csv", lines);
-}
-
-void AddDomainInfo(IList<string> lineParts, string domainName)
-{
-    var ipAddress = Dns.GetHostAddresses(domainName).FirstOrDefault()?.ToString();
-    var whoisInfo = whois.Lookup(domainName);
-    var ipInfo = ipInfoClient.IPApi.GetDetails(ipAddress);
-
-    var asInfo = ipInfo.Org.Split(" ", 2);
-
-    lineParts.Add(domainName);
-    lineParts.Add(whoisInfo?.Registrar?.Name);
-    lineParts.Add(whoisInfo?.Registered == null ? "" : whoisInfo.Registered.Value.ToString("yyyy-MM-dd"));
-    lineParts.Add(ipAddress);
-    lineParts.Add(ipInfo.Country);
-    lineParts.Add(asInfo[0].Replace("AS", ""));
-    lineParts.Add(asInfo[1]);
+    var excludeDomains = File.ReadAllLines(argDictionary["-googleAnnotations"]);
+    googleSearch.GetAnnotationsXml(excludeDomains, "");
 }
---
+++ CyberTools.Files/CsvService.cs
@@ -0,0 +1,81 @@
+﻿using CyberTools.WebSearch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using IPinfo;
+using Whois;
+
+namespace CyberTools.Files;
+public class CsvService
+{
+    private readonly WhoisLookup _whois;
+    private readonly IPinfoClient _ipInfoClient;
+
+    public CsvService()
+    {
+        _whois = new WhoisLookup();
+        var ipInfoToken = "MY_TOKEN"; // TODO: Move to appsettings.json
+        _ipInfoClient = new IPinfoClient.Builder()
+            .AccessToken(ipInfoToken)
+            .Build();
+    }
+
+    public void CreateFromSearch(string searchQuery, string? dateRestrict = null)
+    {
+        var googleSearch = new GoogleSearchService();
+        var lines = new List<string>();
+
+        // Get 100 results, 10 at a time, which is the max google allows.
+        for(var i = 1; i < 91; i += 10)
+        {
+            var googleResults = googleSearch.Search(searchQuery, i, dateRestrict); // use "m6" to get next page of results no older than 6 months
+            if(googleResults.Items == null) { break; }
+            foreach(var item in googleResults.Items)
+            {
+                var lineParts = new List<string?>();
+                var domainName = item.DisplayLink.Replace("www.", "");
+                AddDomainInfo(lineParts, domainName);
+                lineParts.Add("Google Custom Search");
+                lineParts.Add(searchQuery);
+                lineParts.Add(item.Title);
+                lineParts.Add(item.Link);
+
+                lines.Add($"\"{string.Join("\",\"", lineParts)}\"");
+            }
+        }
+        File.WriteAllLines("SearchResults.csv", lines);
+    }
+
+    public void CreateFromDomains(IList<string> domains)
+    {
+        var lines = new List<string>();
+        foreach(var domainName in domains)
+        {
+            var lineParts = new List<string?>();
+            AddDomainInfo(lineParts, domainName);
+
+            lines.Add($"\"{string.Join("\",\"", lineParts)}\"");
+        }
+        File.WriteAllLines("DomainInfo.csv", lines);
+    }
+
+    protected void AddDomainInfo(IList<string> lineParts, string domainName)
+    {
+        var ipAddress = Dns.GetHostAddresses(domainName).FirstOrDefault()?.ToString();
+        var whoisInfo = _whois.Lookup(domainName);
+        var ipInfo = _ipInfoClient.IPApi.GetDetails(ipAddress);
+
+        var asInfo = ipInfo.Org.Split(" ", 2);
+
+        lineParts.Add(domainName);
+        lineParts.Add(whoisInfo?.Registrar?.Name);
+        lineParts.Add(whoisInfo?.Registered == null ? "" : whoisInfo.Registered.Value.ToString("yyyy-MM-dd"));
+        lineParts.Add(ipAddress);
+        lineParts.Add(ipInfo.Country);
+        lineParts.Add(asInfo[0].Replace("AS", ""));
+        lineParts.Add(asInfo[1]);
+    }
+}
